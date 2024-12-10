81ebd1523ecee305119937823fcfc0cd163983de
Phong <phongtrannn3005@gmail.com>
2024-12-11 00:08:49 +0700

    done header

---
+++ components/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../public/css/header.css">
+  <title>Document</title>
+</head>
+<body>
+  <header class="header">
+    <div class="top-bar">
+      <p>One Day Delivery Available</p>
+      <a class= "logReg" href="#" class="login-register">Login / Register</a>
+    </div>
+    <div class="navbar">
+      <div class="logo">
+        <img src="../public/imgs/logo.png" alt="shoe" class="logo-icon">
+      </div>
+      <nav>
+        <ul class="nav-menu">
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">SHOP</a></li>
+          <li><a href="#">PRODUCTS</a></li>
+          <li><a href="#">BLOG</a></li>
+          <li><a href="#">ABOUT US</a></li>
+        </ul>
+      </nav>
+    <div class="icons">
+    <a href="#"><i class="fa fa-search"></i></a>
+    <a href="#"><i class="fa fa-shopping-cart"></i></a>
+    <a href="#"><i class="fa fa-user"></i></a>
+    </div>
+</body>
+</html>
