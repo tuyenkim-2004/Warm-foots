f83d39ac56a84c19c7e57b91d0b455218190dc1c
tuyenkim-2004 <kimtuyen10072004@gmail.com>
2024-12-11 10:03:07 +0700

    update file App in folder core

---
+++ app/core/App.php
@@ -0,0 +1,41 @@
+<?php
+    class App{
+        protected $controller = 'Home';
+        protected $action = 'index';
+        protected $params =[];
+
+
+        function __construct()
+        {
+            $url= $this->urlProcess();
+            if (file_exists('./app/controllers/' . $url[0] . '.php')) {
+                $this->controller= $url[0];
+                unset($url[0]);
+            }
+            require_once './app/controllers'. $this->controller . '.php';
+
+            $this->controller = new $this->controller;
+
+            if(isset($url[1])){
+                if(method_exists($this->controller, $url[1])){
+                    $this->action = $url[1];
+                }
+                unset($url[1]);
+            }
+            
+            $this->params = $url?array_values($url):[];
+
+            call_user_func_array([$this->controller, $this->action], $this->params);
+        }
+
+        function urlProcess(){
+            if(isset($_GET['url'])){
+                return explode('/', filter_var(trim($_GET['url'],'/')));
+            }
+        }
+        
+
+    }
+
+
+?>
