ae6081ac40cf97fdf9b25bd6de7fce84febd81cc
Phong <phongtrannn3005@gmail.com>
2024-12-11 16:26:53 +0700

    done banner

---
+++ app/views/pages/Home.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../../public/css/Home.css">
+</head>
+<body>
+<div class="banner">
+        <img src="../../../public/imgs/slider1.png" alt="Shoe" class="banner-image">
+        <div class="banner-content">
+            <h4>Elevate Your Look</h4>
+            <h1>Find The Perfect Pair <br> Of Shoes To Complete.</h1>
+            <p>Explore our wide range of styles, colors, and materials to find the perfect shoes for any occasion.</p>
+            <a href="#" class="shop-now-btn">Shop now →</a>
+        </div>
+    </div>
+</body>
+</html>
--- components/header.php
+++ components/header.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="../public/css/header.css">
+  <link rel="stylesheet" href="/public/css/header.css">
   <title>Document</title>
 </head>
 <body>
