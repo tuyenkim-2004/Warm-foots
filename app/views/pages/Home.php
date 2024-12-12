e20a23bfc64da8e7d2735d906d91d7cafa553f0a
Phong <phongtrannn3005@gmail.com>
2024-12-12 09:47:14 +0700

    done collection section

--- app/views/pages/Home.php
+++ app/views/pages/Home.php
@@ -5,17 +5,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../../../public/css/Home.css">
+    <!-- <link rel="stylesheet" href="/public/css/Home.css"> -->
+    <link rel="stylesheet" href="/public/css/Home.css">
 </head>
 <body>
-<div class="banner">
-        <img src="../../../public/imgs/slider1.png" alt="Shoe" class="banner-image">
-        <div class="banner-content">
-            <h4>Elevate Your Look</h4>
-            <h1>Find The Perfect Pair <br> Of Shoes To Complete.</h1>
-            <p>Explore our wide range of styles, colors, and materials to find the perfect shoes for any occasion.</p>
-            <a href="#" class="shop-now-btn">Shop now →</a>
+    <div class="banner">
+            <img src="/public/imgs/slider1.png" alt="Shoe" class="banner-image">
+            <div class="banner-content">
+                <h4>Elevate Your Look</h4>
+                <h1>Find The Perfect Pair <br> Of Shoes To Complete.</h1>
+                <p>Explore our wide range of styles, colors, and materials to find the perfect shoes for any occasion.</p>
+                <a href="#" class="shop-now-btn">Shop now →</a>
+            </div>
+    </div>
+
+    <div class="products">
+        <div class="product-item">
+            <img src="/public/imgs/giay1.png" alt="giay nam">
+            <div class="text-overlay">
+                <h2>Trending</h2>
+                <h3>Men Collections</h3>
+                <a href="#" class="shop-now-btn">Shop now →</a>
+            </div>
         </div>
+        <div class="product-item">
+            <img src="/public/imgs/giay2.png" alt="giay nu">
+            <div class="text-overlay">
+                <h2>Latest</h2>
+                <h3>Women Collections</h3>
+                <a href="#" class="shop-now-btn">Shop now →</a>
+            </div>
+        </div>
+
+        <div class="product-item">
+            <img src="/public/imgs/giay3.png" alt="giay tre em">
+            <div class="text-overlay">
+                <h2>Comfort</h2>
+                <h3>Kid Collections</h3>
+                <a href="#" class="shop-now-btn">Shop now →</a>
+            </div>
+        </div>  
     </div>
 </body>
 </html>
