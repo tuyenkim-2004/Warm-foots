e8008ac53071a270b8ee57245770b5d9f47c614f
YMaiNgoc <ymaingoc629@gmail.com>
2024-12-12 13:59:17 +0700

    ngoc commit

--- views/login.php
+++ views/login.php
@@ -4,32 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="../public/css/login.css"> 
+    <link rel="stylesheet" href="../public/css/login.css">
 </head>
 <body>
-    <form action="#">
-    <div class="container">
-        <div class="content">
+    <form action="#" class="container">
             <div class="tittle">
                 <h2 class="text-center">Login</h2>
-                <label for="">Email:</label>
+                <label for="email">Email:</label>
                 <div class="form-group">
-                    <input type="email" class="form-control" name="email" placeholder="Nhập email" required />
+                    <input type="email" id="email" class="form-control" name="email" placeholder="Nhập email" required />
                 </div>
                 <label for="password">Password:</label>
                 <div class="form-group">
-                    <input type="password" class="form-control" name="password" placeholder="Nhập mật khẩu" required />
+                    <input type="password" id="password" class="form-control" name="password" placeholder="Nhập mật khẩu" required />
                 </div>
                 <button type="submit" name="login" class="btn-primary">Login</button>
                 <div class="form-group">
-                    <div class="custom-control custom-checkbox small">
-                        <label for="">New custommer?</label>
-                        <label class="custom-control-label">Register</label>
+                    <div class="custom-control">
+                        <span>New customer?</span>
+                        <a href="#" class="custom-control-link">Register</a>
                     </div>
+                </div>
             </div>
-            </div>
-        </div>
-    </div>
     </form>
 </body>
 </html>
--- views/register.php
+++ views/register.php
@@ -6,49 +6,56 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
     <title>Register</title>
     <link rel="stylesheet" href="../public/css/register.css"> 
+
 </head>
 <body>
-   
-<form action="../controllers/AuthController.php" method="POST">
-    <div class="container">
-        <div class="content">
-            <h2 class="text-center">Register</h2>
-            <div class="frm">
+    <form action="../controllers/AuthController.php" method="POST">
+        <div class="container">
+            <div class="content">
+                <h2 class="text-center">Register</h2>
+                <div class="frm">
+                    <div class="form-group">
+                        <span>Họ và tên:</span>
+                    </div>
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="fullname" placeholder="Nhập họ tên đầy đủ của bạn" required />
+                    </div>
+                </div>
+                <div class="frm">
+                    <div class="form-group">
+                        <span>Email:</span>
+                    </div>
+                    <div class="input-group">
+                        <input type="email" class="form-control" name="email" placeholder="Nhập email" required />
+                    </div>
+                </div>
                 <div class="form-group">
-                    <span >Họ và tên:</span>
-                </div>
-                <div class="input-group">
-                    <input type="text" class="form-control" name="fullname"  placeholder="Nhập họ tên đầy đủ của bạn" required />
-                </div>
-            </div>
-            <div class="frm">
-                <div class="form-group">
-                    <span >Email:</span>
-                </div>
-                <div class="input-group">
-                    <input type="email" class="form-control" name="email"  placeholder="Nhập email" required />
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="input-group">
+                <div class="contain">
                     <span>Nhập mật khẩu:</span>
-                    <input type="password" class="form-control" name="password" placeholder="Nhập mật khẩu" required />
+                    <div class="password-group">
+                        <div class="input-icon mb-3">
+                            <input type="password" class="form-control" id="password" name="password" placeholder="Nhập mật khẩu" required />
+                            <i class="fa-solid fa-key"></i>
+                        </div>
+                    </div>
                 </div>
-                <div class="input-group">
-                    <span >Nhập lại mật khẩu:</span>
-                    <input type="password" class="form-control" name="confirm_password" placeholder="Nhập lại mật khẩu" required />
+                <div class="contain">
+                        <span>Nhập lại mật khẩu:</span>
+                        <div class="password-group">
+                            <div class="input-icon mb-3">
+                                <input type="password" class="form-control" id="password" name="password" placeholder="Nhập mật khẩu" required />
+                                <i class="fa-solid fa-key"></i>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <button type="submit" name="login" class="btn-primary">Login</button>
-            <div class="form-group">
-                <div class="custom-control custom-checkbox small">
+                <button type="submit" name="login" class="btn-primary">Login</button>
+                <div class="custom-control">
                     <label for="">Already have an account?</label>
                     <label class="custom-control-label">Login</label>
                 </div>
             </div>
         </div>
-    </div>
-</form>
-
+    </form>
 </body>
 </html>
